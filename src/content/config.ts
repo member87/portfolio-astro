db6bdfcf3cbb285f94557bb20e1988de07b43a40
member87 <43416057+member87@users.noreply.github.com>
2024-09-17 13:19:18 +0100

    init commit

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    tags: z.string().array().optional(),
+  }),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    tech: z.string().array().optional(),
+    github: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, project };
