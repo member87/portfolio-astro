c2094e1364ead96a865a5550faf97cec79d00695
Jack <43416057+member87@users.noreply.github.com>
2025-03-07 20:50:05 +0000

    add minute read

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,14 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
+import { remarkReadingTime } from "./src/remark-reading-time.mjs";
 
-// https://astro.build/config
 export default defineConfig({
   output: "static",
   site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), icon()],
   markdown: {
+    remarkPlugins: [remarkReadingTime],
     shikiConfig: {
       themes: {
         dark: "material-theme-darker",
